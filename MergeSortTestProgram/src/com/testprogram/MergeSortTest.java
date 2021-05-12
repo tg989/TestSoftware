0facb47cf2eb89e8b059bbee29a76dd7bd239532
tg989 <64399463+tg989@users.noreply.github.com>
2021-05-12 14:57:40 +1000

    Add files via upload

---
+++ MergeSortTestProgram/src/com/testprogram/MergeSortTest.java
@@ -0,0 +1,24 @@
+package com.testprogram;
+
+import org.junit.After;
+import org.junit.Before;
+
+public class MergeSortTest {
+    private MergeSort testSubject;
+
+    @Before
+    public void setUp() throws Exception {
+        this.testSubject = new MergeSort();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        testSubject = null;
+    }
+
+    // Test check unique
+    // Test merge sorted array
+    // Test merge sort
+    // Test merge
+
+}
