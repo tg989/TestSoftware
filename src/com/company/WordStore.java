517d92e04e5e5ae4cac597c36bc16dccdf9056cc
Orbeliss <73449022+Orbeliss@users.noreply.github.com>
2021-05-19 15:11:54 +1000

    Add files via upload

---
+++ src/com/company/WordStore.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.util.Random;
+
+public class WordStore
+{
+    private String word;
+    private int count;
+
+    public WordStore(String word, int count)
+    {
+            this.word = word;
+            this.count = count;
+
+    }
+
+
+    public String getWord()
+    {
+        return word;
+    }
+    public int getCount()
+    {
+        return count;
+    }
+    public void setCount(int number)
+    {
+        count = number;
+    }
+
+}
