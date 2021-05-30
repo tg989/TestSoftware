a8fdf06b9e1ed4afc608751008092ef262171370
tg989 <tg989@uowmail.edu.au>
2021-05-30 16:40:48 +1000

    Cleaned up test runner

--- src/com/testprogram/TestRunner.java
+++ src/com/testprogram/TestRunner.java
@@ -8,51 +8,50 @@ public class TestRunner {
 
     public static void main(String[] args) {
         // Conduct tests & Generate report here
-        if (args[0] == "test") {
-            if(args[1] == "driver") { // Runs only driver tests
-                Result result = JUnitCore.runClasses(DriverTest.class);
-
-                System.out.println("Is the test successful?: " + result.wasSuccessful());
-
-                for (Failure failure : result.getFailures()) {
-                    System.out.println("Failure: " + failure.toString());
-                }
-            } else if(args[1] == "sort") { // Runs only merge sort tests
-                Result result2 = JUnitCore.runClasses(MergeSortTest.class);
-
-                System.out.println("Is the test successful?: " + result2.wasSuccessful());
-
-                for (Failure failure : result2.getFailures()) {
-                    System.out.println("Failure: " + failure.toString());
-                }
-            } else { // Runs Both Test
-                Result result = JUnitCore.runClasses(DriverTest.class);
-
-                System.out.println("Is the test successful?: " + result.wasSuccessful());
-
-                for (Failure failure : result.getFailures()) {
-                    System.out.println("Failure: " + failure.toString());
-                }
-
-                Result result2 = JUnitCore.runClasses(MergeSortTest.class);
-
-                System.out.println("Is the test successful?: " + result2.wasSuccessful());
-
-                for (Failure failure : result2.getFailures()) {
-                    System.out.println("Failure: " + failure.toString());
-                }
+        if(args.length == 0) { // Running Program generally
+            runGenerally();
+        } else if (args[0].equals("test")) {
+            if(args.length == 1) { // Runs Both Test
+                runDriverTests();
+                runSortTests();
+            } else if(args[1].equals("sort")) { // Runs only merge sort tests
+                runSortTests();
+            } else if (args[1].equals("driver")) { // Runs only driver tests
+                runDriverTests();
             }
-        } else { // Running Program generally
-            Driver driver = new Driver();
-            if (driver.readFile()) {
-                System.out.println("RIGHTO");
-            } else {
-                System.out.println("This wrong");
-            }
-            Driver.getMergeSort().mergeSortWordArray();
-            driver.printHead(Driver.getMergeSort().getWordArray(), Driver.getMergeSort().getWordMap());
-            driver.printTail(Driver.getMergeSort().getWordArray(), Driver.getMergeSort().getWordMap(), Driver.getMergeSort().getIndex());
         }
 
+    }
+
+    public static void runGenerally() {
+        Driver driver = new Driver();
+        if (driver.readFile()) {
+            System.out.println("RIGHTO");
+        } else {
+            System.out.println("This wrong");
+        }
+        Driver.getMergeSort().mergeSortWordArray();
+        driver.printHead(Driver.getMergeSort().getWordArray(), Driver.getMergeSort().getWordMap());
+        driver.printTail(Driver.getMergeSort().getWordArray(), Driver.getMergeSort().getWordMap(), Driver.getMergeSort().getIndex());
+    }
+
+    public static void runDriverTests() {
+        Result result = JUnitCore.runClasses(DriverTest.class);
+
+        System.out.println("Is the test successful?: " + result.wasSuccessful());
+
+        for (Failure failure : result.getFailures()) {
+            System.out.println("Failure: " + failure.toString());
+        }
+    }
+
+    public static void runSortTests() {
+        Result result2 = JUnitCore.runClasses(MergeSortTest.class);
+
+        System.out.println("Is the test successful?: " + result2.wasSuccessful());
+
+        for (Failure failure : result2.getFailures()) {
+            System.out.println("Failure: " + failure.toString());
+        }
     }
 }
